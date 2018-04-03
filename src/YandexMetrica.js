dd67f4a133a62df3b86a59b6fed6ab34e510e40f
YANEL\Yanel <romaneskul.yanel@gmail.com>
2018-04-03 21:35:26 +0300

    add reportEvent with params, reportError

--- src/YandexMetrica.js
+++ src/YandexMetrica.js
@@ -5,10 +5,25 @@ export class YandexMetrica {
   static activateWithApiKey(apiKey) {
     YandexMetricaBridgeNative.activateWithApiKey(apiKey);
   }
+  
+   /**
+     * Sends a custom event message and additional parameters (optional).
+     * @param {string} message
+     * @param {object} [params=null]
+     */
+  static reportEvent(message: string, params: ?Object = null) {
+	YandexMetricaBridgeNative.reportEvent(message, params);
+  },
 
-  static reportEvent(evt) {
-    YandexMetricaBridgeNative.reportEvent(evt);
-  }
+  
+  /**
+     * Sends error with reason.
+     * @param {string} error
+     * @param {object} reason
+     */
+   static reportError(error: string, reason: Object) {
+    YandexMetricaBridgeNative.reportError(error, reason);
+   },
 
   static setDryRun(enabled) {
     YandexMetricaBridgeNative.setDryRun(enabled);
