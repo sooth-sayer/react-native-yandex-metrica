6b2201237ca83391fb9e6f6720174c54d35925a1
YANEL\Yanel <romaneskul.yanel@gmail.com>
2018-04-03 22:21:38 +0300

    fix small build error

--- src/YandexMetrica.js
+++ src/YandexMetrica.js
@@ -1,29 +1,29 @@
-import { YandexMetricaBridge as YandexMetricaBridgeNative } from './NativeModule';
+import {YandexMetricaBridge as YandexMetricaBridgeNative} from './NativeModule';
 
 export class YandexMetrica {
 
   static activateWithApiKey(apiKey) {
     YandexMetricaBridgeNative.activateWithApiKey(apiKey);
   }
-  
-   /**
-     * Sends a custom event message and additional parameters (optional).
-     * @param {string} message
-     * @param {object} [params=null]
-     */
-  static reportEvent(message: string, params: ?Object = null) {
-	YandexMetricaBridgeNative.reportEvent(message, params);
-  },
 
-  
   /**
-     * Sends error with reason.
-     * @param {string} error
-     * @param {object} reason
-     */
-   static reportError(error: string, reason: Object) {
+   * Sends a custom event message and additional parameters (optional).
+   * @param {string} message
+   * @param {object} [params=null]
+   */
+  static reportEvent(message: string, params: ?Object = null) {
+    YandexMetricaBridgeNative.reportEvent(message, params);
+  }
+
+
+  /**
+   * Sends error with reason.
+   * @param {string} error
+   * @param {object} reason
+   */
+  static reportError(error: string, reason: Object) {
     YandexMetricaBridgeNative.reportError(error, reason);
-   },
+  }
 
   static setDryRun(enabled) {
     YandexMetricaBridgeNative.setDryRun(enabled);
