0a0aced012b536bdb9ae48ea16789f947dd05d32
YANEL\Yanel <romaneskul.yanel@gmail.com>
2018-08-14 11:17:19 +0300

    update Yandex SDK to 2.9.8

---
+++ ios/yandex-metrica-lib/YandexMobileMetrica-umbrella.h
@@ -0,0 +1,14 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "YandexMobileMetrica.h"
+#import "YMMVersion.h"
+#import "YMMYandexMetrica.h"
+#import "YMMYandexMetricaConfiguration.h"
+#import "YMMYandexMetricaPreloadInfo.h"
+#import "YMMYandexMetricaReporting.h"
+
+FOUNDATION_EXPORT double YandexMobileMetricaVersionNumber;
+FOUNDATION_EXPORT const unsigned char YandexMobileMetricaVersionString[];
+
